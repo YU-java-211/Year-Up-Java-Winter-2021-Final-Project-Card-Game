a9f9fb42837f93aaff6267e2b2e0d26db778d12d
Lylla <72465355+sipaslyl@users.noreply.github.com>
2021-03-15 12:57:19 -0700

    Adds 4 players and gives 13 cards per player

---
+++ src/main/java/Thirteen.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Thirteen {
+    private Scanner scnr;
+    private ArrayList<Player> players;
+    private Hand discardPile;
+    private Card threeOfSpades;
+
+    public Thirteen(){
+        threeOfSpades = new Card(3, 3);
+        Deck deck = new Deck("Deck");
+        deck.shuffle();
+
+        players = new ArrayList<Player>();
+        scnr = new Scanner(System.in);
+
+        System.out.println("Please enter the name of each of your players on a seperate line");
+
+        for (int i = 0; i < 4; i++) {
+            if(scnr.hasNextLine()){
+                String playerName = scnr.nextLine();
+                players.add((new Player(playerName)));
+            }
+            
+        }
+
+        for (Player player : players) {
+            deck.deal(player.getHand(), 13);
+        }
+
+    }
+
+    public void displayState() {
+        for (Player player : players) {
+            player.display();
+        }
+    }
+
+    public boolean isDone() {
+        for (Player player : players) {
+            if (player.getHand().isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Player nextPlayer(Player current) {
+
+        if (players.indexOf(current) == players.size() - 1) {
+            return players.get(0);
+        }
+
+        int playerIndex = players.indexOf(current);
+
+        if (playerIndex != -1) {
+            return players.get(playerIndex + 1);
+        }
+        else {
+            System.out.println("Oh no, we lost a player!!");
+            // build better default later!!
+            return current;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Thirteen game = new Thirteen();
+        //System.out.println(threeOfSpades);
+        game.displayState();
+        
+        
+        
+        //game.playGame();
+    }
+}
