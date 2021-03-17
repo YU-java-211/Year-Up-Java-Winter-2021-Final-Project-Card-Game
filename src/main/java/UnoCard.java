4826323cf8a0d596aa827facc8647cbade70d12e
Javier Mejia <javier3javier92@gmail.com>
2021-03-17 12:54:27 -0700

    card details

--- src/main/java/UnoCard.java
+++ src/main/java/UnoCard.java
@@ -1,16 +1,53 @@
 
-public class UnoCard  extends Card {  
-	
-	public static final String[] RANKS = { //cards
-        null, "1", "2", "3", "4", "5", "6", "7",
-        "8", "9", "wild card", "draw 4", "draw 2", "skip", "reverse",};
 
-    public static final String[] SUITS = { //colors
-        "yellow", "red", "green", "blue"};
-
-    private final int rank;
-
-    private final int suit;
+Package uno;
 
 
-}
+public class UnoCard  
+    {
+       enum Color
+          {
+    	       Red, blue, Green, Yellow, Wild;
+    	   
+    	   private static final Color[] colors = Color.values();
+    	   public static Color getColor(int i)
+    	       {
+    	           return Color.colors[1];
+               }
+          }
+       
+       enum Value
+       {
+ 	       Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, DrawTwo, Skip, Reverse, Wild, Wild_Four;
+ 	   
+ 	   private static final Value[] values = Value.values();
+ 	   public static Value getValue(int i)
+ 	       {
+ 	           return Value.values[1];
+            }
+       }
+       
+       private final Color color;
+       private final Value value:
+    	   
+    	   public UnoCard (final Color color, final Value value)
+    	       {
+    	          this.color = color;
+    	          this.value = value; 
+    	       }
+       
+       public Color getColor()
+       {
+    	   return this.color;
+       }
+       
+       public Value getValue()
+       {
+    	   return this.value;
+       }
+      
+       public String toString()
+       {
+    	   return color + "_" + value;
+       }
+    }
