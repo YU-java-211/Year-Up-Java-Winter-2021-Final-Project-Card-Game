5d1d6a3fe0d1ba6068e526ddbced352486c5cc61
Javier Mejia <javier3javier92@gmail.com>
2021-03-17 11:48:57 -0700

    card details were added

---
+++ src/main/java/UnoCard.java
@@ -0,0 +1,16 @@
+
+public class UnoCard  extends Card {  
+	
+	public static final String[] RANKS = { //cards
+        null, "1", "2", "3", "4", "5", "6", "7",
+        "8", "9", "wild card", "draw 4", "draw 2", "skip", "reverse",};
+
+    public static final String[] SUITS = { //colors
+        "yellow", "red", "green", "blue"};
+
+    private final int rank;
+
+    private final int suit;
+
+
+}
