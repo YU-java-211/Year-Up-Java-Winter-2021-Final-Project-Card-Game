5215f93433b5ef6344165e1026ff67ada64ffa50
AChishungu <72464199+AChishungu@users.noreply.github.com>
2021-03-17 10:47:44 -0700

    my project is not yet finish so people should not use it yet. I added some piles to it

---
+++ src/main/java/Solitair.java
@@ -0,0 +1,9 @@
+public class Solitair {
+
+	
+	private static final int width = 600;
+	private static final int height = 500;
+	private static final String title = "Solitaire";
+	private static final tring
+	
+}
