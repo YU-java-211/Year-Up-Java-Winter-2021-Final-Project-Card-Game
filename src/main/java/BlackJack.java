04b08a16a888069ff5f5de7f742e7569627a5751
mthornez <mthorne@sea.yearup.org>
2021-03-19 20:31:56 -0700

    Finished Display class and main class.

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -4,85 +4,62 @@ import java.util.Scanner;
 public class BlackJack{
 	 private Player livePlayer;
 	 private Player dealer;
-	 private Hand drawPile;
-	 private Hand discardPile;
 	 private Scanner in;
 	 private int pointPlayer; 
 	 private int pointDealer;
+	 private Deck deck;
 	
 	public BlackJack() {
-	        Deck deck = new Deck("Deck");
+	        deck = new Deck("Deck");
 	        deck.shuffle();
-
-	        // set up Scanner
-	        in = new Scanner(System.in);
 
 	        livePlayer = new Player("Player");
 	        dealer = new Player("Dealer");
 
 	        deck.deal(livePlayer.getHand(), 2);
 	        deck.deal(dealer.getHand(), 2);
-	        
-
-	        // put the rest of the deck face down
-	        drawPile = new Hand("Draw pile");
-	        deck.dealAll(drawPile);
-
-	        // create the scanner we'll use to wait for the user
-	        //in = new Scanner(System.in);
-	        
 	    }
 	
 	 public void displayState() {
 	        System.out.print("Draw pile: ");
-	        System.out.println(drawPile.size() + " cards");
 	        System.out.println("Player has ");
 	        
         	pointPlayer = 0;
         	int aces = 0; 
         	Hand playerHand = livePlayer.getHand();
-        	for (int i = 0; i < playerHand.cards.size(); i++) {
-        		
-        	}
+        	
 	        for (Card card : playerHand.cards) {
 	        	if (card.toString().indexOf("Ace") > -1) {
 	        		aces++;
 	        	}
-	        	System.out.println(card); 
-	        	pointPlayer += card.getRank();
+		        System.out.println(card); 
+		        pointPlayer += card.getRank();
 	        }
-	        /* Disabling for easier automation
-	        in.nextLine();
-	        */
+	        
 	        System.out.println(pointPlayer + aces + " or " + pointPlayer + (aces * 11));
 	        
-	        pointDealer = 1; 
-	        dCard1 = 0;
-	        dCard2 = 1; 
+	        pointDealer = 0; 
 	        Hand dealerHand = dealer.getHand();
-	        for (int i = 0; i < dealerHand.cards.size(); i++) {
-	        	
+	        for (int i = 1; i < dealerHand.cards.size(); i++) {
+	        	Card card = dealerHand.getCard(i); 
+	        	System.out.println(dealerHand.getCard(i));
+	 	        if (card.toString().indexOf("Ace") > -1) {
+	 	        		aces++; 
+	 	        }
+	 	        pointDealer += card.getRank();
 	        }
-	        for (Card card : playerHand.cards) {
-	        	if (card.toString().indexOf("Ace") > -1) {
-	        		aces++; 
-	        	}
-	        	System.out.println(card); 
-	        	pointDealer += card.getRank();
-	        }
-	        System.out.println(pointDealer + aces + " or " + pointDealer + (aces * 11));
-	        // one card facedown
-	        // reset values to default 
+	        
+	        System.out.println(pointDealer + aces + " or " + pointDealer + (aces * 11) + " showing, one card face down.");
 	    }
 	 
+	
+	 
 	 public void playGame() {
-		  	System.out.println("Dealing set up");
-		  	
+		  
 	    }
 	 
 	 public static void main(String[] args) {
-		 	
-	 
+	      BlackJack game = new BlackJack();
+	      game.playGame();
 	 }
-	
 }
