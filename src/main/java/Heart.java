2c8b8e88165afa68aa77d3d32187ed3ebb7ac40f
azdela <72465332+azdela@users.noreply.github.com>
2021-03-22 00:52:31 -0700

    Update Heart.java
    Create methods such as ending the game, prompt for adding player's name and displaying each player's hand.

--- src/main/java/Heart.java
+++ src/main/java/Heart.java
@@ -10,3 +10,46 @@ public class Heart {
     private Card twoOfClubs;
     private Player lastPlayer;
     private int score;
+
+public Heart() {
+        twoOfClubs = new Card(2, 0);
+        Deck deck = new Deck("Deck");
+        discardPile = new Hand("Discards");
+        deck.shuffle();
+
+        players = new ArrayList<Player>();
+        console = new Scanner(System.in);
+
+        System.out.println("Please enter the name of each of your players on a seperate line");
+
+        for (int i = 0; i < 4; i++) {
+            if (console.hasNextLine()) {
+                String playerName = console.nextLine();
+                players.add((new Player(playerName)));
+                System.out.println("Player " + (i + 1) + ": " + playerName);
+            }
+        }
+        
+        System.out.println();
+        System.out.println("In hearts, you must play the 2 of Clubs first.");
+        System.out.println();
+
+        for (Player player : players) {
+            deck.deal(player.getHand(), 13);
+        }
+    }
+
+    public void displayState() {
+        for (Player player : players) {
+            player.display();
+        }
+    }
+
+    public boolean isDone() {      
+      for (Player player : players) {
+         if (player.getHand().isEmpty()) {
+            return true;
+         }
+      }
+      return false;
+    }
