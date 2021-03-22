aef45ffdea43b11a34b6e5fecd5bf57482c616da
rzkxaa <72465685+rzkxaa@users.noreply.github.com>
2021-03-22 00:51:02 -0700

    Update Heart.java
    Creates card and fields

--- src/main/java/Heart.java
+++ src/main/java/Heart.java
@@ -2,3 +2,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.*;
+
+public class Heart {
+    private Scanner console;
+    private ArrayList<Player> players;
+    private Hand discardPile;
+    private Card twoOfClubs;
+    private Player lastPlayer;
+    private int score;
