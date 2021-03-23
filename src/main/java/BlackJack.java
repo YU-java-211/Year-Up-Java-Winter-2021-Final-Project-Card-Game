cffa82bbc13b9185d61cc64732f297801eeb8d74
LopeznoeGH <72465512+LopeznoeGH@users.noreply.github.com>
2021-03-22 18:37:06 -0700

    Update BlackJack.java

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,73 +1,95 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import java.util.List;
 
-public class BlackJack{
-	 private Player livePlayer;
-	 private Player dealer;
-	 private Scanner in;
-	 private int pointPlayer; 
-	 private int pointDealer;
-	 private Deck deck;
-	
-	public BlackJack() {
-	        deck = new Deck("Deck");
-	        deck.shuffle();
+public class BlackJack {
+    private Scanner console;
+    private ArrayList<Player> players;
+    private int score;
+    private int score1;
+    private int score2;
+    private Player winner;
 
-	        livePlayer = new Player("Player");
-	        dealer = new Player("Dealer");
+    public BlackJack() {
+        Deck deck = new Deck("Deck");
+        deck.shuffle();
 
-	        deck.deal(livePlayer.getHand(), 2);
-	        deck.deal(dealer.getHand(), 2);
-	    }
-	
-	 public void displayStatePlayer() {
-	        System.out.print("Draw pile: ");
-	        System.out.println("Player has ");
-	        
-        	pointPlayer = 0;
-        	int aces = 0; 
-        	Hand playerHand = livePlayer.getHand();
-        	
-	        for (Card card : playerHand.cards) {
-	        	if (card.toString().indexOf("Ace") > -1) {
-	        		aces++;
-	        	}
-		        System.out.println(card); 
-		        pointPlayer += card.getRank();
-	        }
-	        
-	        int smallPointPlayer = pointPlayer + aces;
-	        int largePointPlayer = pointPlayer + (aces * 11);
-	        
-	        System.out.println(smallPointPlayer + " or " + largePointPlayer);
-	 }
-	 
-	 public void displayStateDealer() {
-	        
-	        pointDealer = 0; 
-	        int aces = 0; 
-	        Hand dealerHand = dealer.getHand();
-	        for (int i = 1; i < dealerHand.cards.size(); i++) {
-	        	Card card = dealerHand.getCard(i); 
-	        	System.out.println(dealerHand.getCard(i));
-	 	        if (card.toString().indexOf("Ace") > -1) {
-	 	        		aces++; 
-	 	        }
-	 	        pointDealer += card.getRank();
-	        }
-	        
-	        int smallPointDealer = pointDealer + aces; 
-	        int largePointDealer = pointDealer + (aces * 11); 
-	        
-	        System.out.println(smallPointDealer + " or " + largePointDealer + " showing, one card face down.");
-	    }
-	 
-	 public void playGame() {
-		  
-	    }
-	 
-	 public static void main(String[] args) {
-	      BlackJack game = new BlackJack();
-	      game.playGame();
-	 }
+        players = new ArrayList<Player>();
+        console = new Scanner(System.in);
+        
+        System.out.println("Welcome to a game of BlackJack! Whoever's hand is equal to or closest to 21 wins!");
+        System.out.println();
+
+
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+               System.out.print("Please enter player's name: ");
+            } else if (i == 1) {
+               System.out.println();
+               System.out.print("Please enter dealer's name: ");
+            }
+            if (console.hasNextLine()) {
+                String playerName = console.nextLine();
+                players.add((new Player(playerName)));
+                System.out.println("Player " + (i + 1) + ": " + playerName);
+            }
+        }
+        for (Player player : players) {
+               deck.deal(player.getHand(), 3);
+        }
+    }
+
+   public void displayState() {
+      for (Player player : players) {
+         player.display();
+      }
+   }
+   
+   public int logic(Player player) {
+      Hand currentHand = player.getHand();
+      score = 0;
+      for (int i = 0; i < currentHand.size(); i++) {
+         Card currentCard = currentHand.getCard(i);
+         if (currentCard.getRank() == 11 || currentCard.getRank() == 12 || currentCard.getRank() == 13) {
+            score += 10;
+         } else if ((currentCard.getRank() + score) == 21) {
+            winner = player;
+            score += currentCard.getRank();
+            break;
+         } else {
+            score += currentCard.getRank();
+         }
+      }
+      return score;
+   }
+ 
+    public void gameplay() {
+      Player playerTurn = players.get(0);
+      Player player1 = playerTurn;
+      score1 = logic(playerTurn);
+      playerTurn = players.get(1);
+      Player player2 = playerTurn;
+      score2 = logic(playerTurn);
+      if (winner != null) {
+         System.out.println("The winner is " + winner.getName());
+      } else if (score1 > score2 && score1 <= 21 || score2 > 21) {
+         System.out.println("The winner is " + player1.getName());
+      } else if (score2 > score1 && score2 <= 21 || score1 > 21) {
+         System.out.println("The winner is " + player2.getName());
+      } else {
+         System.out.println("It's a tie");
+      }
+      System.out.println();
+      System.out.println(player1.getName() + "'s score: " + score1);
+      System.out.println(player2.getName() + "'s score: " + score2);
+    }
+
+   public static void main(String[] args) {
+      BlackJack game = new BlackJack();
+      System.out.println();
+      game.displayState();
+      game.gameplay();
+   }
 }
